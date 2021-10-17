f5ac6ee008392f3fe43fd8d854366e05c492f287
HamzaVedat <agZ2UebT3C6G8aR>
2021-10-17 22:37:01 +0300

    Database table add

--- Takip Programı/Models/TakipProgramiContext.cs
+++ Takip Programı/Models/TakipProgramiContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,25 @@ namespace Takip_Programı.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=TakipProgramiDb.db");
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=TakipProgramiDb.db"))   
+            using (SQLiteCommand command = con.CreateCommand())
+            {
+                con.Open();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'Customer' ('Id' INTEGER NOT NULL, 'Name' TEXT, 'Surname' TEXT, 'Risk' INTEGER NOT NULL, 'Change'  INTEGER NOT NULL, 'Position' TEXT, 'Phone'  TEXT, 'MobilePhone' TEXT, 'VergiDairesi'    TEXT, 'VergiNo' TEXT, 'Adress'  TEXT, CONSTRAINT 'PK_Customer' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
+             
+                /*command.CommandText  = "SELECT name FROM sqlite_master WHERE name='User2'";
+                var name = command.ExecuteScalar();
+                if (name == null)
+                {
+                  
+                    
+                    command.CommandText = "INSERT 'User2' ('Id' INTEGER NOT NULL, 'Username' TEXT, 'Password' TEXT, 'Role' TEXT, CONSTRAINT 'PK_User' PRIMARY KEY('Id' AUTOINCREMENT) )";
+                    command.ExecuteNonQuery();
+                }*/
+            }
+
+           
         }
         public DbSet<User> User { get; set; }
         public DbSet<Customer> Customer { get; set; }
