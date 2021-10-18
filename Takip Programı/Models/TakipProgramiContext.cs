5473844889d014772c58c1243546e178a3c5cef6
Hamza Vedat Cengiz <hamzavedatcengizbg@outlook.com>
2021-10-18 13:50:10 +0300

    db düzenleme

--- Takip Programı/Models/TakipProgramiContext.cs
+++ Takip Programı/Models/TakipProgramiContext.cs
@@ -19,7 +19,16 @@ namespace Takip_Programı.Models
                 con.Open();
                 command.CommandText = "CREATE TABLE IF NOT EXISTS 'Customer' ('Id' INTEGER NOT NULL, 'Name' TEXT, 'Surname' TEXT, 'Risk' INTEGER NOT NULL, 'Change'  INTEGER NOT NULL, 'Position' TEXT, 'Phone'  TEXT, 'MobilePhone' TEXT, 'VergiDairesi'    TEXT, 'VergiNo' TEXT, 'Adress'  TEXT, CONSTRAINT 'PK_Customer' PRIMARY KEY('Id' AUTOINCREMENT))";
                 command.ExecuteNonQuery();
-             
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'ProductDefine' ( 'Id' INTEGER NOT NULL, 'Name' TEXT, 'Type' TEXT, 'Amount' REAL NOT NULL, 'BuyPrice' REAL NOT NULL, 'SellPrice' REAL NOT NULL, 'BuyAmount' REAL NOT NULL, 'SellAmount' REAL NOT NULL, CONSTRAINT 'PK_ProductDefine' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'PumpDefine'('Id' INTEGER NOT NULL, 'InitialCounter' REAL NOT NULL, 'PumpName' TEXT, 'WareHouseId' INTEGER, 'Counter' REAL, CONSTRAINT 'FK_PumpDefine_WareHouseDefine_WareHouseId' FOREIGN KEY('WareHouseId') REFERENCES 'WareHouseDefine'('Id') ON DELETE RESTRICT, CONSTRAINT 'PK_PumpDefine' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'PumpFormModel' ( 'Id' INTEGER NOT NULL, 'WorkDate' TEXT NOT NULL, 'PersonName' TEXT, 'FirstCounter' TEXT, 'LastCounter' TEXT, 'Difference' TEXT, 'Amount' TEXT, 'PumpDefineId' INTEGER, 'Price' NUMERIC, CONSTRAINT 'FK_PumpFormModel_PumpDefine_PumpDefineId' FOREIGN KEY('PumpDefineId') REFERENCES 'PumpDefine'('Id') ON DELETE RESTRICT, CONSTRAINT 'PK_PumpFormModel' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'User' ( 'Id' INTEGER NOT NULL, 'Username' TEXT, 'Password' TEXT, 'Role' TEXT, CONSTRAINT 'PK_User' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS 'WareHouseDefine' ( 'Id' INTEGER NOT NULL, 'WareHouseName' TEXT, 'WareHouseVolume' NUMERIC NOT NULL, 'WareHouseStock' NUMERIC NOT NULL, 'ProductDefineId' INTEGER, CONSTRAINT 'FK_WareHouseDefine_ProductDefine_ProductDefineId' FOREIGN KEY('ProductDefineId') REFERENCES 'ProductDefine'('Id') ON DELETE RESTRICT, CONSTRAINT 'PK_WareHouseDefine' PRIMARY KEY('Id' AUTOINCREMENT))";
+                command.ExecuteNonQuery();
                 /*command.CommandText  = "SELECT name FROM sqlite_master WHERE name='User2'";
                 var name = command.ExecuteScalar();
                 if (name == null)
